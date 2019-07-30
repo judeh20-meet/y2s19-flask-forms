c5fe9db8a7c60d110722653fbb78deab8f3ef9d5
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:28:07 -0400

    AMALAMALAMALMALAMAL

--- app.py
+++ app.py
@@ -12,5 +12,19 @@ def display_student(student_id):
 
 #Create an '/add' route here:
 
+@app.route("/add", methods =["GET", "POST"])
+def add_student_route():
+	if request.method == "GET":
+		return render_template("add.html")
+	else:
+		name = request.form["student_name"]
+		year = request.form["student_year"]
+		finished_lab = request.form["finished_lab"]
+		finished_lab=finished_lab=="True"
+
+		add_student(name,year,finished_lab)
+		print("Recieved POST request!")
+		return render_template("add.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
